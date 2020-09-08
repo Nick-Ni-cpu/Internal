51b327482de993176061571f9a373658d27e2196
Nick-Ni-cpu <18477@jhc.school.nzjhc.school.nz>
2020-09-08 10:31:57 +0800

    Show ids in showall.php

--- showall.php
+++ showall.php
@@ -1,6 +1,13 @@
 <?php include('topbit.php'); 
 
-	$find_sql = 'SELECT * FROM `wine_data`';
+	$find_sql = "SELECT * FROM `wine_data`
+	JOIN Variety ON (wine_data.VarietyID = variety.VarietyID)
+	JOIN Winery ON (wine_data.WineryID = winery.WineryID)
+	JOIN Designation ON (wine_data.DesignationID = designation.DesignationID)
+	JOIN Province ON (wine_data.ProvinceID = province.ProvinceID)
+	JOIN Taster_name ON (wine_data.Taster_nameID = taster_name.Taster_nameID)
+	JOIN Taster_twitter_handle ON (wine_data.Taster_twitter_handleID = taster_twitter_handle.Taster_twitter_handleID)
+	";
 	$find_query = mysqli_query($dbconnect, $find_sql);
 	$find_rs = mysqli_fetch_assoc($find_query);
 	$count = mysqli_num_rows($find_query);
@@ -37,8 +44,34 @@
 					</span>
 					
 					<br />
-					<?php echo $find_rs["VarietyID"];?>
+					<b>Variety</b>:
 					<?php echo $find_rs["Variety"];?>
+					
+					<br />
+					
+					<b>Winery</b>:
+					<?php echo $find_rs["Winery"];?>
+					
+					<br />
+					
+					<b>Designation</b>:
+					<?php echo $find_rs["Designation"];?>
+					
+					<br />
+					
+					<b>Province</b>:
+					<?php echo $find_rs["Province"];?>
+					
+					<br />
+					
+					<b>Taster_name</b>:
+					<?php echo $find_rs["Taster_name"];?>
+					
+					<br />
+					
+					<b>Taster_twitter_handle</b>:
+					<?php echo $find_rs["Taster_twitter_handle"];?>
+					
 				</div> <!--/results-->
 				
 				<br />
@@ -54,13 +87,7 @@
 	
 				?>
 				
-<?php include('topbit.php'); ?>			
-			<div class="box main">
-				<h2>All Results</h2>
-				
-				<p>
-					Results will go here...
-				</p>
+
 
 			</div> <!-- / main -->
 			
